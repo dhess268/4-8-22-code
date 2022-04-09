b626cd2f52cf4feb81b36e8ca789588988faa101
Dillon Hess <dhess268@gmail.com>
2022-04-09 11:54:19 -0400

    add api functionality to get genders

--- daily.js
+++ daily.js
@@ -1,14 +1,26 @@
 
 
 // fetches super mario 64 70 star speedrun world record
+let playerID = ''
+
 
 fetch('https://www.speedrun.com/api/v1/leaderboards/o1y9wo6q/category/7dgrrxk4?top=1')
     .then(res =>{
         return res.json()
     })
     .then(data =>{
-        console.log(data)
-    })
+        // playerID = data.runs[0].run.players[0].id
+        // console.log(data.data.runs[0].run.players[0].id)
+        playerID = data.data.runs[0].run.players[0].id
+        let url = "https://www.speedrun.com/api/v1/users/" + playerID
+        fetch(url)
+            .then(res =>{
+                return res.json()
+            })
+            .then(data =>{
+                console.log(data.data.pronouns)
+            })
+            })
     .catch(err => {
         console.log(`error ${err}`)
     })
